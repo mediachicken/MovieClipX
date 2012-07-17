48a86ef99a7ca8e3cbde3becb99fde1c125df13e
Garet McKinley <garetmckinley@me.com>
2012-07-17 10:50:17 -0400

    updated to allow development using the prototype/ folder

--- build-examples.py
+++ build-examples.py
@@ -2,13 +2,19 @@
 ## by placing the current "mcx.lua" file into
 ## all of the example projects.
 
+# note: this script has only been tested on Mac OSX
+
 import os, shutil
 
-library = "mcx.lua"
+library = "prototype/mcx.lua"
 
 for path, subdirs, files in os.walk("examples"):
 	if len(subdirs) > 0:
 		examples = subdirs
 		
 for folder in examples:
-	shutil.copyfile("mcx.lua", "examples/" + folder + "/mcx.lua")
+	shutil.copyfile(library, "examples/" + folder + "/mcx.lua")
+	
+
+#uncomment the lines below when using the prototype/ folder for development
+shutil.copyfile(library, "mcx.lua")
